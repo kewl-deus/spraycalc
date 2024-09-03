3b05839c810f4794cd0d64186bd4080784b1ee7c
kewl-deus <deus.mail@web.de>
2024-09-03 15:48:35 +0200

    config for hosting with path "/spraycalc"

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,39 @@
-import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
+import {VitePWA} from 'vite-plugin-pwa';
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    registerType: 'autoUpdate',
-    injectRegister: 'script',
+    base: '/spraycalc/',
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            injectRegister: 'script',
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+            pwaAssets: {
+                disabled: false,
+                config: true,
+            },
 
-    manifest: {
-      name: 'Spray Calculator',
-      short_name: 'Spray Calc',
-      description: 'Spray Calculation App',
-      theme_color: '#673ab8',
-    },
+            manifest: {
+                name: 'Spray Calculator',
+                short_name: 'Spray Calc',
+                description: 'Spray Calculation App',
+                theme_color: '#673ab8',
+            },
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+                cleanupOutdatedCaches: true,
+                clientsClaim: true,
+            },
 
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
+            devOptions: {
+                enabled: false,
+                navigateFallback: 'index.html',
+                suppressWarnings: true,
+                type: 'module',
+            },
+        })],
 })
